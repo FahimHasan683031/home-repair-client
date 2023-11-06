7ee70ccd71c19a47ee7afda7f0a80a68d404b07d
FahimHasan683031 <fahimhasan683031@gmail.com>
2023-11-06 15:19:41 +0600

    creat login and register page and connect with firebase

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -66,9 +66,9 @@ const Signup = () => {
             <div className="hero min-h-screen bg-base-200">
                 <div className="hero-content flex-col w-full md:w-4/6 lg:w-1/3">
 
-                    <div className=" flex-shrink-0 w-full max-w-sm bg-purple-200 shadow-xl ">
+                    <div className=" flex-shrink-0 w-full max-w-sm bg-teal-100 shadow-xl ">
                         <form onSubmit={registerHandle} className=" px-6 pt-4 pb-6 text-sm ">
-                            <h1 className="text-4xl text-purple-700 pt-3 font-bold text-center mb-4">Register now</h1>
+                            <h1 className="text-4xl text-teal-600 pt-3 font-bold text-center mb-4">Register now</h1>
                             <div className="form-control">
                                 <label className="label">
                                     <span className="label-text">Name</span>
@@ -107,7 +107,7 @@ const Signup = () => {
                                 <p>Already have account ? Please <Link className="text-blue-700" to='/login'>Login</Link></p>
                             </div>
                             <div className="form-control mt-4">
-                                <button type="submit" className="py-3 text-white font-bold bg-purple-700">Register</button>
+                                <button type="submit" className="py-3 text-white font-bold bg-teal-700">Register</button>
                             </div>
                             <div className="text-center border-t border-slate-600 my-6 pt-3">
                                 <FcGoogle onClick={googleSigninHandle} className="text-3xl cursor-pointer mx-auto"></FcGoogle>
