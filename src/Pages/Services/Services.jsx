80cf2203475d98b9a171dd5a20291b1feb664de5
FahimHasan683031 <fahimhasan683031@gmail.com>
2023-11-07 03:19:33 +0600

    add services search functionality in theis project

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -1,32 +1,44 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import ServiceCard from "../../Components/ServiceCard";
+import { AiOutlineSearch } from "react-icons/ai";
 const Services = () => {
     const [limit, setLimit] = useState(6)
     const [services, setServices] = useState([])
-    const [searchText,setSearchText]=useState('')
+    const [searchText, setSearchText] = useState('')
     useEffect(() => {
         axios.get(`http://localhost:5000/api/v1/services?limit=${limit}&serviceName=${searchText}`)
             .then(data => setServices(data.data))
-    }, [limit,searchText])
-    const searchInputHandle =e=>{
+    }, [limit, searchText])
+    const searchInputHandle = e => {
         const value = e.target.value
         setSearchText(value)
     }
     return (
-        <div className="max-w-screen-xl mx-auto px-6">
-            this is services
-            <input onChange={searchInputHandle} type="text" name="search" id="" />
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
-                {
-                    services?.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
-                }
-            </div>
-            <div className={`text-center ${limit === 0 ? 'hidden' : ''}`}>
+        <div>
+            <div className="bg-[url('https://i.ibb.co/bQ1JWBp/mp-our-services-mobile.png')]  relative bg-no-repeat bg-cover bg-top py-20">
+                <div className="absolute h-full w-full top-0 z-0 left-0 bg-[#080808a8]"></div>
+                <div className="text-center relative z-20">
+                    <h1 className="text-5xl mb-3 font-bold text-teal-500">Our Services</h1>
+                    <input className="w-1/3 px-3 py-[9px] rounded-sm my-5 " placeholder="Search Services By Name..." onChange={searchInputHandle} type="text" name="search" id="" />
+                    <AiOutlineSearch className="text-teal-500 text-3xl absolute top-[87px] right-[460px]"></AiOutlineSearch>
+                </div>
 
-                
-            <button onClick={() => setLimit(0)} className="px-3 py-2 mb-12 font-medium text-white bg-teal-600 rounded mx-auto">View All</button>
-                
+            </div>
+            <div className="max-w-screen-xl mt-16 mx-auto px-6">
+
+
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 my-8">
+                    {
+                        services?.map(service => <ServiceCard key={service._id} service={service}></ServiceCard>)
+                    }
+                </div>
+                <div className={`text-center ${limit === 0 ? 'hidden' : ''}`}>
+
+
+                    <button onClick={() => setLimit(0)} className="px-3 py-2 mb-12 font-medium text-white bg-teal-600 rounded mx-auto">View All</button>
+
+                </div>
             </div>
         </div>
     );
